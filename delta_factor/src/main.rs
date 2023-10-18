014aeb0ec3821ce42af921eb91c1f1acc321e29d
first_user <adityapatil24680@gmail.com>
2023-10-18 23:28:17 +0530

    delta_factor work started

--- delta_factor/src/main.rs
+++ delta_factor/src/main.rs
@@ -1,3 +1,14 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead}
+
+fn extract_data(dataset_file_path: String) -> Result<Vec<Vec<i32>>, Box<dyn Error>> {
+    let file = File::open(dataset_file_path)?;
+    let reader = io::BufReader::newfile(file);
+
+    // to be continued
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Success!");
 }
