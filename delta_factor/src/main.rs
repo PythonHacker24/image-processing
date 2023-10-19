55a9c247de1eb858f0189d07f9eb7f183a4cfedf
first_user <adityapatil24680@gmail.com>
2023-10-19 10:32:17 +0530

    data extractor initiated

--- delta_factor/src/main.rs
+++ delta_factor/src/main.rs
@@ -6,9 +6,26 @@ fn extract_data(dataset_file_path: String) -> Result<Vec<Vec<i32>>, Box<dyn Erro
     let file = File::open(dataset_file_path)?;
     let reader = io::BufReader::newfile(file);
 
-    // to be continued
+    let mut output_vector: Vec<Vec<i32>> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let mut numbers: Vec<i32> = Vec::new();
+
+        for num_str in line.split(',') {
+            if let Ok(num) = num_str.trim().parse() {
+                numbers.push(num);
+            }
+        }
+        output_vector.push(numbers);
+    }
+    Ok(output_vector);
 }
 
 fn main() {
-    println!("Success!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.is_empty() {
+        println!("[*] Path to dataset not provided");
+        std::process::exit(1);
+    }
 }
