6f525fb84327d3518221c61565b41dc521998e16
first_user <adityapatil24680@gmail.com>
2023-10-19 18:01:21 +0530

    some graphing added error fixed

--- input_processing/src/main.rs
+++ input_processing/src/main.rs
@@ -69,7 +69,7 @@ fn main() {
 
     let image_linear_vector = linear_data_of_image(image_path);
     for element in image_linear_vector {
-        for _ in element {
+        for _ in 0..=element {
             print!("|");
         }
         print!("\n");
